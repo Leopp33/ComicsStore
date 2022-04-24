77e200780f31d1a5bba2faa4794279ca29b51037
Leopp33 <leopp3.3@gmail.com>
2022-04-24 16:35:27 -0500

    feature: Se añadió soporte básico para variables de entorno y se crearon componentes para cada operación que se va a realizar.

--- comics_store/comics_store/settings.py
+++ comics_store/comics_store/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kjft3j9k7@9n)%orjv20^qv@o-b)(wf&z+0^$1hse*(tqh_b(1'
+SECRET_KEY = 'django-insecure-340ea(oxm&bh+wf4*cl%vhbsxgcwvh-1##wthm(qmi+iyfr=8-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
