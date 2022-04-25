3d2eef03e78a3be5d282cbb302fea4207c4f09eb
Leopp33 <leopp3.3@gmail.com>
2022-04-25 01:50:58 -0500

    feature: Se añadió funcionalidad parcial a searchComics

--- comics_store/searchComics/views.py
+++ comics_store/searchComics/views.py
@@ -1,3 +1,13 @@
+from urllib import response
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+# import asyncio
+import requests
+
+def index(request):
+    
+    response = requests.get('https://gateway.marvel.com/v1/public/comics?ts=1&apikey=6b458513b5383de9cf55d82c26551dd1&hash=eb6f264d37e1fd74ad010baa8b900005')
+    data_set = response.json()
+
+    return JsonResponse(data_set)
